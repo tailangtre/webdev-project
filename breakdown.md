af261db15a81dda3a423fbed0a010af9bd6fe263
tomtran <rotquankiama@gmail.com>
2024-12-02 13:20:59 -0800

    Add edit and delete features for reminders

---
+++ breakdown.md
@@ -0,0 +1,13 @@
+Nov 18th, 2024
+Tom Tran:
+I worked on the following tasks:
+1. make the delete work
+2. add the ability to edit a reminder
+
+Nov 25th, 2024
+Tom Tran:
+I worked on the following tasks:
+1. Add signup feature
+2. Add login feature
+
+Dec 2nd, 2024
--- controller/reminder_controller.js
+++ controller/reminder_controller.js
@@ -26,7 +26,7 @@ let remindersController = {
       id: database.cindy.reminders.length + 1,
       title: req.body.title,
       description: req.body.description,
-      completed: false,
+      completed: "false",
     };
     database.cindy.reminders.push(reminder);
     res.redirect("/reminders");
@@ -42,10 +42,23 @@ let remindersController = {
 
   update: (req, res) => {
     // implement this code
+    findReminder = database.cindy.reminders.find(function (reminder) {
+      return reminder.id == req.params.id;
+    });
+    findReminder.title = req.body.title;
+    findReminder.description = req.body.description;
+    findReminder.completed = req.body.completed;
+    console.log(findReminder);
+    res.redirect("/reminder/" + findReminder.id);
   },
 
   delete: (req, res) => {
     // Implement this code
+    findReminder = database.cindy.reminders.find(function (reminder) {
+      return reminder.id == req.params.id;
+    });
+    database.cindy.reminders.splice(database.cindy.reminders.indexOf(findReminder), 1);
+    res.redirect("/reminders");
   },
 };
 
