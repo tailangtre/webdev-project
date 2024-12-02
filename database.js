d807456c391b64a46582cc05288af7e786877473
tomtran <rotquankiama@gmail.com>
2024-12-02 08:54:09 -0800

    first commit: add starter code

---
+++ database.js
@@ -0,0 +1,17 @@
+let Database = {
+  cindy: {
+    reminders: [
+      {
+        id: 1,
+        title: "buy milk",
+        description: "go to safeway and buy milk",
+        completed: false,
+      },
+    ],
+  },
+  alex: {
+    reminders: [],
+  },
+};
+
+module.exports = Database;
